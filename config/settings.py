949d12eb5c9b51bda74c587c4cf676afcb800308
Artem Matrashov <artem.m2013@gmail.com>
2023-08-30 19:53:02 +0300

    can't complete now

--- config/settings.py
+++ config/settings.py
@@ -118,6 +118,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    # Здесь вы можете добавить пути к вашим статическим файлам,
+    # если они располагаются вне приложений
+    BASE_DIR / 'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
